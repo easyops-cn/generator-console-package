f56739b01f4f546d0fd1c1abb06acf4156c8f842
weareoutman <wangshenwei@qq.com>
2018-11-23 15:46:56 +0800

    feat: support console-plugins/@plugin-common
    Closes #2

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,21 +1,72 @@
+const fs = require('fs');
 const Generator = require('yeoman-generator');
 const yosay = require('yosay');
 
+const scopePropsMap = new Map();
+scopePropsMap.set('@easyops', {
+  repository: 'Console-W',
+  subPackagePath: 'packages'
+});
+scopePropsMap.set('@plugin-common', {
+  repository: 'console-plugins',
+  subPackagePath: '@plugin-common'
+});
+const scopes = Array.from(scopePropsMap.keys());
+
 module.exports = class extends Generator {
   initializing() {
+    const done = this.async();
     this.log(yosay('Welcome to the Console Package generator!'));
     this.conflicter.force = true;
+    fs.readFile('package.json', {
+      encoding: 'utf8'
+    }, (err, content) => {
+      if (err) {
+        this.log('Warning: can\'t find package.json in current directory');
+      } else {
+        switch (JSON.parse(content).name) {
+          case 'easyops-web-console':
+            this.defaultScopeIndex = 0;
+            break;
+          case 'console-plugins':
+            this.defaultScopeIndex = 1;
+            break;
+          default:
+            this.log('Warning: current directory seems incorrect');
+        }
+      }
+      done();
+    });
   }
 
   async prompting() {
-    // package name
+    // scope
     this.props = await this.prompt([
       {
-        type: 'input',
-        name: 'packageName',
-        message: "What's the name of your package (in kebab-case)?"
+        type: 'list',
+        name: 'scope',
+        message: "What's the scope of your package?",
+        choices: scopes,
+        default: this.defaultScopeIndex
       }
     ]);
+    const { scope } = this.props;
+
+    // repository
+    Object.assign(this.props, scopePropsMap.get(scope));
+
+    // package name
+    Object.assign(
+      this.props,
+      await this.prompt([
+        {
+          type: 'input',
+          name: 'packageName',
+          message: "What's the name of your package (in kebab-case)?",
+          validate: value => /^[a-zA-Z]+(-[a-zA-Z0-9]+)*$/.test(value)
+        }
+      ])
+    );
     const { packageName } = this.props;
 
     // module name
@@ -54,8 +105,8 @@ module.exports = class extends Generator {
 
   writing() {
     const srcPath = this.sourceRoot();
-    const { packageName, componentName } = this.props;
-    const destPath = 'packages/' + packageName;
+    const { packageName, componentName, scope, subPackagePath } = this.props;
+    const destPath = subPackagePath + '/' + packageName;
     this.fs.copy(
       srcPath + '/public_api.ts',
       destPath + '/public_api.ts'
@@ -104,7 +155,7 @@ module.exports = class extends Generator {
     this.fs.copy(tsconfigPath, tsconfigPath, {
       process: content => {
         const tsconfig = JSON.parse(content);
-        tsconfig.compilerOptions.paths[`@easyops/${packageName}`] = [`packages/${packageName}`];
+        tsconfig.compilerOptions.paths[`${scope}/${packageName}`] = [`${subPackagePath}/${packageName}`];
         return JSON.stringify(tsconfig, null, '  ') + '\n';
       }
     });
@@ -114,27 +165,27 @@ module.exports = class extends Generator {
       process: content => {
         const angular = JSON.parse(content);
         angular.projects[packageName] = {
-          "root": `packages/${packageName}/src`,
+          "root": `${subPackagePath}/${packageName}/src`,
           "projectType": "application",
           "schematics": {
             "@schematics/angular:component": {
-              "path": `packages/${packageName}/src`,
+              "path": `${subPackagePath}/${packageName}/src`,
               "styleext": "scss"
             },
             "@schematics/angular:directive": {
-              "path": `packages/${packageName}/src`
+              "path": `${subPackagePath}/${packageName}/src`
             },
             "@schematics/angular:module": {
-              "path": `packages/${packageName}/src`
+              "path": `${subPackagePath}/${packageName}/src`
             },
             "@schematics/angular:service":{
-              "path": `packages/${packageName}/src`
+              "path": `${subPackagePath}/${packageName}/src`
             },
             "@schematics/angular:pipe": {
-              "path": `packages/${packageName}/src`
+              "path": `${subPackagePath}/${packageName}/src`
             },
             "@schematics/angular:class": {
-              "path": `packages/${packageName}/src`,
+              "path": `${subPackagePath}/${packageName}/src`,
               "spec": true
             }
           }
@@ -146,7 +197,7 @@ module.exports = class extends Generator {
 
   install() {
     const done = this.async();
-    const distPath = 'packages/' + this.props.packageName + '/dist';
+    const distPath = this.props.subPackagePath + '/' + this.props.packageName + '/dist';
     const child = this.spawnCommand('yarn', ['link'], { cwd: distPath });
     child.on("close", () => {
       done();
