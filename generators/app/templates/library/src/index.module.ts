4842f5b56572e8d9f4f2f4eb83e2124fa6ada8e0
weareoutman <wangshenwei@qq.com>
2019-03-01 11:20:26 +0800

    fix: reduce dependencies in templates

--- generators/app/templates/library/src/index.module.ts
+++ generators/app/templates/library/src/index.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { Extension } from "@easyops/console-plugin-core";
 import { <%= componentClassName %> } from "./components/<%= componentName %>.component";
 
-@Extension("<%= scope %>/<%= packageName %>")
 @NgModule({
   imports: [CommonModule],
   declarations: [
